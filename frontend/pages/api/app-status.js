25223cd8d1dabee1dd5a313e725d5b9a7109b63a
seeubot <155578467+seeubot@users.noreply.github.com>
2026-09-17 11:50:50 +0530

    Update app-status.js

--- frontend/pages/api/app-status.js
+++ frontend/pages/api/app-status.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   res.status(200).json({
     success: true,
     data: {
-      maintenanceMode: false,
+      maintenanceMode: true,
       forceUpdate: false,
       latestVersion: '1.0.0',
       minSupportedVersion: '1.0.0',
